1629fa32541ee3265c9435bf807d7d51d10032a0
Marcin Gromek <marcin.gromek@wundermanthompson.com>
2022-01-21 15:43:46 +0100

    bugfix: Fix issue when trying to remove ingredient (by clicking 'less' button)

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -6,15 +6,16 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import classes from './Burger.module.css';
 import classesGlobal from '../../App.module.css';
 
-function Burger({ ingredients, error }) {
+function Burger({ ingredients, hasError }) {
   let ingredientsAll = Object.keys(ingredients)
-    .map(igKey => [...Array(Math.max(0, ingredients[igKey]))].map((_, i) => <BurgerIngredient key={igKey + i} type={igKey} />))
-    .reduce((arr, curr) => arr.concat(curr), []);
+    .map(ingredientsKey => [...Array(Math.max(0, ingredients[ingredientsKey]))]
+      .map((_, i) => <BurgerIngredient key={ingredientsKey + i} type={ingredientsKey} />))
+    	.reduce((arr, curr) => arr.concat(curr), []);
 
   if (ingredientsAll.length === 0) {
     ingredientsAll = (
       <p className={classesGlobal.TextCenter} style={{ fontWeight: '600' }}>
-        {!error ? 'Add ingredients, please' : 'Sorry, something went wrong'}
+        {!hasError ? 'Add ingredients, please' : 'Sorry, something went wrong'}
       </p>
     );
   }
@@ -31,5 +32,6 @@ function Burger({ ingredients, error }) {
 export default withRouter(Burger);
 
 Burger.propTypes = {
-  ingredients: PropTypes.object.isRequired
+  ingredients: PropTypes.object.isRequired,
+  hasError: PropTypes.bool.isRequired
 };
