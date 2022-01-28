798d37bae8d70c1796b1652bbc09c51f81dece2d
Marcin Gromek <marcin.gromek@wundermanthompson.com>
2022-01-28 17:00:21 +0100

    feature: Add PrivateRoute component for preventing user to display content without login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import './App.module.css';
-import { Redirect, Route, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import React, { useContext } from 'react';
 import Login from './components/Login/Login';
-import AuthContext from './store/auth-context';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import Layout from './hoc/Layout/Layout';
 import Orders from './containers/Orders/Orders';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
 
 function App() {
-  const { isLoggedIn } = useContext(AuthContext);
-
   return (
     <Layout>
       <Switch>
         <Route component={Login} path="/login" />
         <Route component={Orders} path="/orders" />
         {/* eslint-disable-next-line react/no-children-prop */}
-        <Route children={() => (isLoggedIn ? <BurgerBuilder /> : <Redirect to="/login" />)} path="/" />
+        <PrivateRoute path="/" children={<BurgerBuilder />} redirectTo="/login" />
         <Route component={Checkout} path="/checkout" />
-        <Route path="/" render={() => (!isLoggedIn ? <Redirect to="/login" /> : <Redirect to="/" />)} />
       </Switch>
     </Layout>
   );
