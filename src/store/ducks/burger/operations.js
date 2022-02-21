335336d5a7c634dad9bbbebf482fc7e601ea3401
Marcin Gromek <marcin.gromek@wundermanthompson.com>
2022-02-21 17:31:12 +0100

    feature: Implement Re-ducks for burger builder state management

---
+++ src/store/ducks/burger/operations.js
@@ -0,0 +1,13 @@
+import actions from './actions';
+
+const setIngredients = actions.setIngredients;
+const setTotalPrice = actions.setTotalPrice;
+const updatePurchase = actions.updatePurchase;
+const isPurchasing = actions.isPurchasing;
+
+export default {
+  setIngredients,
+  setTotalPrice,
+  updatePurchase,
+  isPurchasing
+};
