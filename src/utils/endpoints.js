c8d960cc6fbee09e2bb1020aa33b1d6482670c29
Marcin Gromek <gromson101@gmail.com>
2023-03-02 10:57:35 +0100

    refactor: uppercase api keys

--- src/utils/endpoints.js
+++ src/utils/endpoints.js
@@ -1,3 +1,3 @@
-export const FIREBASE_SIGN_IN_WITH_PASSWORD = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env.REACT_APP_apiKey_firebase}`;
-export const FIREBASE_SIGN_UP = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${process.env.REACT_APP_apiKey_firebase}`;
-export const FIREBASE_UPDATE_PASSWORD = `https://identitytoolkit.googleapis.com/v1/accounts:update?key=${process.env.REACT_APP_apiKey_firebase}`;
+export const FIREBASE_SIGN_IN_WITH_PASSWORD = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env.REACT_APP_API_KEY_FIREBASE}`;
+export const FIREBASE_SIGN_UP = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${process.env.REACT_APP_API_KEY_FIREBASE}`;
+export const FIREBASE_UPDATE_PASSWORD = `https://identitytoolkit.googleapis.com/v1/accounts:update?key=${process.env.REACT_APP_API_KEY_FIREBASE}`;
