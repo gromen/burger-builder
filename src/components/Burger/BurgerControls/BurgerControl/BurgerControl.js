87003547e6ee165e5fff115bb0e0677d948203a2
Marcin Gromek <gromson101@gmail.com>
2023-03-02 10:05:14 +0100

    refactor(eslint): clean files to meet eslint requirements

--- src/components/Burger/BurgerControls/BurgerControl/BurgerControl.js
+++ src/components/Burger/BurgerControls/BurgerControl/BurgerControl.js
@@ -2,12 +2,15 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import classes from './BurgerControl.module.css';
 
-const BurgerControl = ({
-  label, added, removed, disabledNote
-}) => (
+const BurgerControl = ({ label, added, removed, disabledNote }) => (
   <div className={classes.BuildControl}>
     <div className={classes.Label}>{label}</div>
-    <button type="button" className={classes.More} onClick={added} title={`Add ${label} ingredient`}>
+    <button
+      type="button"
+      className={classes.More}
+      onClick={added}
+      title={`Add ${label} ingredient`}
+    >
       &#43;
     </button>
 
@@ -29,7 +32,7 @@ BurgerControl.propTypes = {
   removed: PropTypes.func.isRequired,
   added: PropTypes.func.isRequired,
   disabledNote: PropTypes.bool,
-  label: PropTypes.string,
+  label: PropTypes.string
 };
 
 BurgerControl.defaultProps = {
