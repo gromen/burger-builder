227aae5657c1cdecd76a76a786749f918dcd83ff
Marcin Gromek <gromson101@gmail.com>
2023-03-02 14:04:24 +0100

    refactor(typescript): rewrite CheckoutSummary to typescript

--- src/components/Order/CheckoutSummary/CheckoutSummary.tsx
+++ src/components/Order/CheckoutSummary/CheckoutSummary.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 import Burger from '../../Burger/Burger';
 
 import classes from './CheckoutSummary.module.css';
+import { type Ingredients } from '../../../utils/ingredientPrices';
 
-function checkoutSummary({ ingredients }) {
+interface PropsCheckoutSummary {
+  ingredients: Ingredients;
+}
+
+function checkoutSummary({ ingredients }: PropsCheckoutSummary): JSX.Element {
   return (
     <div className={classes.CheckoutSummary}>
       <h1>Checkout Summary</h1>
