5e5e729f0577238dc99de0a011d69a1f56c66f5a
Marcin Gromek <gromson101@gmail.com>
2023-03-06 09:59:42 +0100

    refactor(typescript): rewrite UserProfilePage to typescript

--- src/containers/UserProfilePage/UserProfilePage.tsx
+++ src/containers/UserProfilePage/UserProfilePage.tsx
@@ -1,23 +1,22 @@
-import React, { useRef } from 'react';
-import {
-  Button, Col, Container, Form, Row
-} from 'react-bootstrap';
-import { useSelector } from 'react-redux';
+import React, { type FormEvent, useRef } from 'react';
+import { Button, Col, Container, Form, Row } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import { FIREBASE_UPDATE_PASSWORD } from '../../utils/endpoints';
+import { useAppSelector } from '../../hooks/redux-toolkit';
 
-const UserProfilePage = () => {
-  const idToken = useSelector(state => state.userAuthState.idToken);
-  const passwordFieldRef = useRef();
+const UserProfilePage = (): JSX.Element => {
+  const idToken = useAppSelector((state) => state.userAuthState.idToken);
+  const passwordFieldRef = useRef<HTMLInputElement>(null);
   const navigate = useNavigate();
 
   let enteredNewPassword = passwordFieldRef.current;
 
-  const onSubmit = event => {
+  const onSubmit = (event: FormEvent): void => {
     event.preventDefault();
-    enteredNewPassword = passwordFieldRef?.current?.value;
+    // @ts-expect-error to fix
+    enteredNewPassword = passwordFieldRef.current?.value;
 
-    fetch(FIREBASE_UPDATE_PASSWORD, {
+    void fetch(FIREBASE_UPDATE_PASSWORD, {
       method: 'POST',
       body: JSON.stringify({
         idToken,
@@ -27,7 +26,7 @@ const UserProfilePage = () => {
       headers: {
         'Content-type': 'application/json'
       }
-    }).then(response => {
+    }).then(() => {
       navigate('/', { replace: true });
     });
   };
@@ -49,15 +48,12 @@ const UserProfilePage = () => {
                 aria-describedby="passwordHint"
               />
               <Form.Text id="passwordHint" muted>
-                Your password must greater or equal 8 characters long, contain one lowercase and one uppercase character
+                Your password must greater or equal 8 characters long, contain
+                one lowercase and one uppercase character
               </Form.Text>
               <Form.Control.Feedback as="small" type="invalid">
-                At least one lowercase letter
-                {' '}
-                <br />
-                The total length should be greater than or equal to 8
-                {' '}
-                <br />
+                At least one lowercase letter <br />
+                The total length should be greater than or equal to 8 <br />
               </Form.Control.Feedback>
             </Form.Group>
             <Button className="w-100" variant="primary" type="submit">
