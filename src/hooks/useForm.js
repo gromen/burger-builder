deb40c162afb31aaa7646bfcd5031908e3720976
Marcin Gromek <gromson101@gmail.com>
2023-11-12 20:57:37 +0100

    fix: resolve type checking in .js files - not needed

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,9 +1,10 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 import { useState } from 'react';
 
 const useForm = (initialValue = {}) => {
   const [formData, setFormData] = useState(initialValue);
 
-  const inputChangeHandler = event => {
+  const inputChangeHandler = (event) => {
     setFormData({
       ...formData,
       [event.target.name]: event.target.value
